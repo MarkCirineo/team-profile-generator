22c573bcf246c8a4becd2564c18871655ded0491
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-02 23:31:34 -0400

    added basic function to choose which employee you want to create

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const inquirer = require("inquirer");
 
 // function with prompt for "engineer" or "intern"
 
+const decideNextQuestions = [
+    {
+        type: "list",
+        message: "'Would you like to create any more employees'",
+        name: "option",
+        choices: ["Yes, create an Engineer", "Yes, create an Intern", "No, I'm finished"]
+    }
+]
+
+const decideNext = () => {
+    inquirer
+        .prompt(decideNextQuestions)
+            .then((data) => {
+                if (data.option === "Yes, create an Engineer") {
+                    console.log(data.option)
+                } else if (data.option === "Yes, create an Intern") {
+                    console.log(data.option)
+                } else {
+                    console.log(data.option)
+                }
+            })
+}
+
 const managerQuestions = [
     {
         type: "input",
@@ -26,6 +49,7 @@ const init = () => {
         .prompt(managerQuestions)
             .then((data) => {
                 console.log(data)
+                decideNext();
             })
 }
 init();
